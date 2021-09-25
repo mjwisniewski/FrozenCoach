418a7d7922c02667106562dd20b035653cf49997
Mark Wisniewski <37741977+mjwisniewski@users.noreply.github.com>
2021-09-25 13:12:21 -0400

    Adding 404.html page per the Angular documentation

--- frozen-coach/src/app/app-routing.module.ts
+++ frozen-coach/src/app/app-routing.module.ts
@@ -7,20 +7,19 @@ import { GameRulesComponent } from './pages/lady-knights/game-rules/game-rules.c
 import { LadyKnightsComponent } from './pages/lady-knights/lady-knights.component';
 
 const routes: Routes = [
+  { path: '', component: FrozenCoachComponent },
   {
     path: 'LadyKnights', component: LadyKnightsComponent, children: [
+      // { path: '', component: LadyKnightsComponent },
       { path: 'About', component: AboutLadyKnightsComponent },
       { path: 'GameRules', component: GameRulesComponent }
-
     ]
   },
   { path: 'About', component: AboutComponent },
-
   // { path: 'about', component: AboutComponent },
   // { path: 'contact', component: ContactComponent },
   // { path: 'admin', component: AdminComponent },
   // { path: 'registration', component: RegistrationComponent },
-  { path: '', component: FrozenCoachComponent },
   { path: '**', component: FrozenCoachComponent }
 ];
 
